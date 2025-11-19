f7ea6aa3e796aa18aac32969212fce1975cb4e06
ShadWeb <shyifou@gmail.com>
2025-11-19 17:01:19 +0330

    up-gitegnore

--- .next/static/chunks/a2c03785de2d819d.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/268fb412645eb591.js",
-  "static/chunks/cfd257739111d1b4.js",
-  "static/chunks/a5631d94ea37f3e4.js",
-  "static/chunks/a0de7eb471f35315.js",
-  "static/chunks/bd00876a53f5c5ec.js",
-  "static/chunks/turbopack-a7af6402e5257e31.js"
-])
